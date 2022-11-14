bdb04ec5d29e6ba526352c466fa2f56521c8cf97
maverickdevop <57834199+maverickdevop@users.noreply.github.com>
2022-11-14 22:40:02 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,44 @@
+""" USER DATA """
+USER = "user"
+PASSWORD = "pass"
+
+""" QUERY SEGMENTS PARAMETERS """
+PROJECT_ID = "6542"
+DAY_FROM = "2022-11-07"
+DAY_TO = "2022-11-07"
+
+SE_Y = "yandex"
+SE_G = "google"
+
+DAY_FROM_PERIOD = "2022-10-07"
+DAY_TO_PERIOD = "2022-11-07"
+
+DOCUMENT = "/yeezy"
+QUERY = "yeezy"
+
+CATEGORY = "384210"
+CATEGORY_NAME = "high top10"
+
+GROUP = "265528"
+GROUP_NAME = "group add seo page"
+
+QUERY_GROUP = "135769"
+QUERY_GROUP_NAME = '"!спрос" от 100 до 1000'
+
+""" FILTER PARAMS """
+WS1_FROM = "100"
+WS1_TO = "200000"
+
+WS3_FROM = "20"
+WS3_TO = "80"
+
+TOP3_FROM = "1"
+TOP3_TO = "55"
+
+PTRAF_FROM = "1"
+PTRAF_TO = "150"
+
+AVG_TOP30_FROM = "3"
+AVG_TOP30_TO = "10"
+
+POS_FROM = "10"
